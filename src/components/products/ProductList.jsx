388ab820e5db1de326210de6768c5a3b936f6c7c
LautaroAcosta1 <acostalauty765@gmail.com>
2024-01-06 12:49:41 -0300

    Se agregan cambios en las clases del archivo productList.

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -19,9 +19,9 @@ const ProductList = () => {
                     <h3 className="card-name">{product.name}</h3>
                     <p className="card-description">{product.description}</p>
                     <h2 className="card-price">${product.price}</h2>
-                    <button type="button" class="button">
-                        <span class="button__text" onClick={() => buyProducts(product)}>Agregar</span>
-                        <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
+                    <button type="button" className="button">
+                        <span className="button__text" onClick={() => buyProducts(product)}>Agregar</span>
+                        <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
                     </button>
                 </div>
         )
