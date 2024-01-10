0549adc05eeabbf2bd065074af16461b21ec2bb4
LautaroAcosta1 <acostalauty765@gmail.com>
2024-01-10 15:47:33 -0300

    Se soluciona error que habia en el boton de 'agregar'.

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -6,9 +6,7 @@ const ProductList = () => {
     const {data, cart, setCart} = useContext(dataContext)
 
     const buyProducts = (product) => {
-        console.log(product)
         setCart([...cart, product])
-        console.log(cart)
     }
 
     return data.map((product) => {
@@ -20,9 +18,14 @@ const ProductList = () => {
                     <h3 className="card-name">{product.name}</h3>
                     <p className="card-description">{product.description}</p>
                     <h2 className="card-price">${product.price}</h2>
-                    <button type="button" className="button">
-                        <span className="button__text" onClick={() => buyProducts(product)}>Agregar</span>
-                        <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
+                    <button type="button" className="button" onClick={() => buyProducts(product)}>
+                        <span className="button__text">Agregar</span>
+                        <span className="button__icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className="svg">
+                                <line y2="19" y1="5" x2="12" x1="12"></line>
+                                <line y2="12" y1="12" x2="19" x1="5"></line>
+                            </svg>
+                        </span>
                     </button>
                 </div>
         )
