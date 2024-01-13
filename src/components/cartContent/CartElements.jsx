f266abe1b7c639f9b70745c571bd7a47cafaa43f
LautaroAcosta1 <acostalauty765@gmail.com>
2024-01-13 14:13:00 -0300

    Se agregan actualizaciones al diseño del cartContent.

--- src/components/cartContent/CartElements.jsx
+++ src/components/cartContent/CartElements.jsx
@@ -19,10 +19,22 @@ const CartElements = () => {
         return (
             <div className="cartContent" key={product.id}>
                 <img className="image-cart" src={product.image} alt={product.name}/>
-                <h3 className="name-cart">{product.name}</h3>
-                <CartItemCounter product={product}/>
-                <h4 className="price-cart">${product.price * product.quantity}</h4>
-                <button id="btnDelete-cart" className="fa-solid fa-trash" onClick={() => deleteProduct(product.id)}></button>
+                <div className="name-cart-container">
+                    <h3 className="name-cart">{product.name}</h3>
+                </div>
+                <div className="cart-itemCounter-container">
+                    <CartItemCounter product={product}/>
+                </div>
+                <div className="price-cart-container">
+                    <h4 className="price-cart">${product.price * product.quantity}</h4>
+                </div>
+                <div className="btnDelete-cart-container">
+                    <button className="btnDelete-cart" onClick={() => deleteProduct(product.id)}>
+                        <svg viewBox="0 0 448 512" className="svgIcon">
+                            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
         )
     })
