1de4a81a2c6d4192263eb9eea70a5241719dc070
LautaroAcosta1 <acostalauty765@gmail.com>
2024-01-13 19:52:08 -0300

    Se agrega boton de 'Iniciar compra'.

--- src/components/cartContent/CartTotal.jsx
+++ src/components/cartContent/CartTotal.jsx
@@ -9,9 +9,15 @@ const CartTotal = () => {
     return (
         <div className="cartTotal">
             <h1 className="cartTotal-text">Resumen de compra</h1>
-            <div class="horizontal-line"></div>
+            <div className="horizontal-line"></div>
             <h2 className="cartTotal-subtotal">SubtotaL ${total}</h2>
             <h3 className="cartTotal-total">Total ${total}</h3>
+            <button className="btnBuy">
+                <span className="btnBuy-circle" aria-hidden="true">
+                    <span className="btnBuy-icon btnBuy-arrow"></span>
+                </span>
+                <span className="btnBuy-text">Iniciar Compra</span>
+            </button>
         </div>
     )
 }
