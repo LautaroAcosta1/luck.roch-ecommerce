a54b488206e62e76aaa3d83f64910c00d330b20b
LautaroAcosta1 <acostalauty765@gmail.com>
2024-01-25 04:33:40 -0300

    Se cambia el diseño del texto en aboutUs.

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
             <img className="aboutUs-img" src="/assets/imagen1.png" alt="Sobre nosotros"/>
             <div className="aboutUs-text-container">
                 <h2 className="aboutUs-title">Sobre Nosotros</h2>
-                <p className="aboutUs-p">En <b>Luck.Roch</b>, celebramos la moda como una expresión de individualidad. Ofrecemos una cuidadosa selección de prendas de calidad que reflejan estilo, comodidad y elegancia. Nuestro compromiso es brindarte una experiencia de compra excepcional, donde la moda se encuentra con la autenticidad de cada persona. Únete a nuestra comunidad y descubre cómo la moda puede realzar tu estilo único en <b>Luck.Roch</b>.</p>
+                <p className="aboutUs-p">En <b>Luck Roch</b>, celebramos la moda como una expresión de individualidad. Ofrecemos una cuidadosa selección de prendas de calidad que reflejan estilo, comodidad y elegancia. Nuestro compromiso es brindarte una experiencia de compra excepcional, donde la moda se encuentra con la autenticidad de cada persona. Únete a nuestra comunidad y descubre cómo la moda puede realzar tu estilo único en <b>Luck Roch</b>.</p>
             </div>
         </div>
     )
